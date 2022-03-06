1dc18483734da87eeeea9c9b9e0cc98a257ee840
VOsipenkov <VOsipenkov@lanit.ru>
2022-03-07 02:21:18 +0300

    /design thymeleaf html

--- src/main/java/com/example/tacocloud/model/Taco.java
+++ src/main/java/com/example/tacocloud/model/Taco.java
@@ -3,10 +3,8 @@ package com.example.tacocloud.model;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
 @Data
 @RequiredArgsConstructor
 public class Taco {
-    private List<Ingredient> ingredientList;
+    private String name;
 }
