9db75ee88f7252d60fc1f51b224c12835fccc9e7
VOsipenkov <VOsipenkov@lanit.ru>
2022-03-16 03:04:30 +0300

    added in-memory user store for spring-security

--- src/main/java/com/example/tacocloud/config/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/config/SecurityConfig.java
@@ -1,10 +1,28 @@
 package com.example.tacocloud.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
+//     An in-memory user store
+//     A JDBC-based user store
+//     An LDAP-backed user store
+//     A custom user details service
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+            .withUser("buzz")
+            .password("infinity")
+            .roles("USER")
+            .and()
+            .withUser("woody")
+            .password("bullseye")
+            .roles("USER");
+    }
 }
