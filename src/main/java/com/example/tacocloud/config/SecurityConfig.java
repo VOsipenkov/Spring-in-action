63478aaf366f27021b15c5a418c24d8d42297fd7
VOsipenkov <VOsipenkov@lanit.ru>
2022-03-23 14:33:06 +0300

    disable csrf temporarily

--- src/main/java/com/example/tacocloud/config/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/config/SecurityConfig.java
@@ -45,6 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .loginPage("/login")
             .and()
             .logout()
-            .logoutSuccessUrl("/");
+            .logoutSuccessUrl("/")
+            .and()
+            .csrf().disable();
     }
 }
